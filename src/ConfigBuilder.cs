af7375927cf4bce7d4691bf10ccc795ff62e0d83
Markus Keil <markus.keil@slock.it>
2019-03-14 09:24:08 +0100

    move config stuff to own class

---
+++ src/ConfigBuilder.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.IO;
+using src.Models;
+
+namespace src
+{
+    public static class ConfigBuilder
+    {
+        private static string GetConfig(IDictionary env,string name, string defaultValue)
+        {
+            string value = (string) env[name];
+            return string.IsNullOrWhiteSpace(value) ? defaultValue : value;
+        }
+        
+        public static UpdateWatchOptions BuildConfigurationFromEnvironment(IDictionary env)
+        {
+            string contractAddresss = GetConfig(env,"CONTRACT_ADDRESS",String.Empty);
+            string stackPath = GetConfig(env,"STACK_PATH","./demo-stack");
+            string rpcEndpoint = GetConfig(env,"RPC_ENDPOINT","http://localhost:8545");
+            string validatorAddress = GetConfig(env,"VALIDATOR_ADDRESS",String.Empty);
+            
+            return new UpdateWatchOptions
+            {
+                RpcEndpoint = rpcEndpoint,
+                ContractAddress = contractAddresss,
+                ValidatorAddress = validatorAddress,
+                DockerStackPath = stackPath,
+                ConfigurationProvider = new ConfigurationFileHandler(Path.Combine(stackPath, ".env")),
+                MessageService = new ConsoleMessageService(),
+                DockerComposeControl = new LinuxComposeControl()
+            };
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.IO;
+using System.Collections;
+using Docker.DotNet.Models;
 using Nethereum.Contracts;
 using src;
 using src.Interfaces;
@@ -9,33 +10,15 @@ namespace src
 {
     internal static class Program
     {
-        private static string _contractAddresss;
-        private static string _stackPath;
-        private static string _rpcEndpoint;
-        private static string _validatorAddress;
-
+        
         private static void Main(string[] args)
         {
             Console.WriteLine("EWF NodeControl");
-
-            // config stuff
-            _contractAddresss = "0x";
-            _stackPath = "./demo-stack";
-            _rpcEndpoint = "http://localhost:8545";
-            _validatorAddress = "0xc3681dfe99730eb45154208cba7b0df7e705f305";
-
-            // fill the options
-            UpdateWatchOptions watchOpts = new UpdateWatchOptions
-            {
-                RpcEndpoint = _rpcEndpoint,
-                ContractAddress = _contractAddresss,
-                ValidatorAddress = _validatorAddress,
-                DockerStackPath = _stackPath,
-                ConfigurationProvider = new ConfigurationFileHandler(Path.Combine(_stackPath, ".env")),
-                MessageService = new ConsoleMessageService(),
-                DockerComposeControl = new LinuxComposeControl()
-            };
             
+            // config stuff
+            IDictionary env = Environment.GetEnvironmentVariables();
+            UpdateWatchOptions watchOpts = ConfigBuilder.BuildConfigurationFromEnvironment(env);
+
             // instantiate the update watch
             UpdateWatch uw = new UpdateWatch(watchOpts);
             
@@ -45,7 +28,6 @@ namespace src
             // start watching - Will block here
             uw.StartWatch();
 
-            
         }
     }
 }
