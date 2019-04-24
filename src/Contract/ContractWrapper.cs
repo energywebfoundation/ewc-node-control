cc770c544cc3176d5f5e936e0f899cf2fcd04076
Markus Keil <markus.keil@slock.it>
2019-04-24 23:02:52 +0200

    switch to self sign

--- src/Contract/ContractWrapper.cs
+++ src/Contract/ContractWrapper.cs
@@ -10,6 +10,7 @@ using Nethereum.Hex.HexTypes;
 using Nethereum.RPC.Eth.Blocks;
 using Nethereum.RPC.Eth.DTOs;
 using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
 using Nethereum.Web3.Accounts.Managed;
 using src.Interfaces;
 using src.Models;
@@ -55,13 +56,14 @@ namespace src.Contract
         /// <param name="lookupContractAddress">Address of the address lookup smart contract</param>
         /// <param name="rpcEndpoint">HTTP URL to the JSON-RPC endpoint</param>
         /// <param name="validatorAddress">The ethereum address of the controlled validator</param>
-        public ContractWrapper(string lookupContractAddress, string rpcEndpoint, string validatorAddress, ILogger logger,string keyPw)
+        public ContractWrapper(string lookupContractAddress, string rpcEndpoint, string validatorAddress, ILogger logger,string keyPw,string keyJson = "")
         {
             _validatorAddress = validatorAddress;
             _logger = logger;
             
             // load the validator account
-            ManagedAccount acc = new ManagedAccount(validatorAddress,keyPw);
+            //ManagedAccount acc = new ManagedAccount(validatorAddress,keyPw);
+            Account acc = Account.LoadFromKeyStore(keyJson, keyPw);
             
             // create a web 3 instance
             
--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading;
 using Docker.DotNet.Models;
 using Nethereum.Contracts;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using src;
 using src.Contract;
 using src.Interfaces;
@@ -30,13 +34,27 @@ namespace src
                 logger.Log("Unable to read secret. Exiting.");
                 return;
             }
-
+           
             string keyPw = File.ReadAllText(secretPath);
+
+            string encKey;
+            // Read the encrpyted key from parity
+            using (HttpClient hc = new HttpClient())
+            {
+                var sc = new StringContent(
+                    $"{{ \"method\": \"parity_exportAccount\", \"params\": [\"{watchOpts.ValidatorAddress}\",\" {keyPw} \"], \"id\": 1, \"jsonrpc\": \"2.0\" }}");
+                sc.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+                var response = hc.PostAsync(watchOpts.RpcEndpoint, sc).Result;
+                var resContent = response.Content.ReadAsStringAsync().Result;
+                JObject resObj = JObject.Parse(resContent);
+                encKey = resObj["result"].ToString();
+            }
+                
             
             // Add dependencies
             watchOpts.ConfigurationProvider = new ConfigurationFileHandler(Path.Combine(watchOpts.DockerStackPath, ".env"));
             watchOpts.DockerControl = new LinuxDockerControl(logger);
-            watchOpts.ContractWrapper = new ContractWrapper(watchOpts.ContractAddress,watchOpts.RpcEndpoint,watchOpts.ValidatorAddress,logger,keyPw);
+            watchOpts.ContractWrapper = new ContractWrapper(watchOpts.ContractAddress,watchOpts.RpcEndpoint,watchOpts.ValidatorAddress,logger,keyPw,encKey);
 
             // instantiate the update watch
             UpdateWatch uw = new UpdateWatch(watchOpts,logger);
